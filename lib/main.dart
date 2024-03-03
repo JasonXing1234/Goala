d3a155fe996d54b1f2599ca3327d5514a3da4747
JasonXing1234 <113735814+JasonXing1234@users.noreply.github.com>
2024-03-02 17:02:21 -0800

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:Goala/state/profile_state.dart';
 import 'package:Goala/state/suggestionUserState.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
